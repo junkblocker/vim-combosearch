cddaea0d6cfb0dc2899f7b32a367db23d2822e86
Manpreet Singh <junkblocker@yahoo.com>
2021-06-13 09:31:17 -0700

    Accumulated preferences

--- plugin/search.sh
+++ plugin/search.sh
@@ -8,34 +8,35 @@ black="$(tput setaf 0)"
 red="$(tput setaf 1)"
 green="$(tput setaf 2)"
 orange="$(tput setaf 3)"
+blue="$(tput setaf 4)"
 reset="$(tput sgr0)"
 
 colorize_file_output() {
-	LC_ALL=C sed -E "s/^(.*)(:[0-9]+:[0-9]+)/$orange\1$black\2$reset/g"
+    LC_ALL=C sed -E "s/^(.*)(:[0-9]+:[0-9]+)/$orange\1$black\2$reset/g"
 }
 
 colorize_output() {
-	LC_ALL=C sed -E "s|^(.*):([0-9]+):([0-9]+)(:)|$red\1$reset:$green\2$(tput sgr0):$reset\3\4 |g"
+    LC_ALL=C sed -E "s|^(.*):([0-9]+):([0-9]+)(:)|$red\1$reset:$green\2$(tput sgr0):$reset\3\4 |g"
 }
 
 callgitgrepcommand() {
-	git --no-pager ls-files -cmo --exclude-standard ":(icase)**$SEARCH_QUERY**" | LC_ALL=C sed "s/$/:0:0:/g" | colorize_file_output
-	awk '!x[$0]++' <(
-		(git --no-pager grep -I -n -i --untracked "$SEARCH_QUERY" | LC_ALL=C sed -E "s/:([0-9]+):/:\1:0:/g" | colorize_output)
-	)
+    git --no-pager ls-files -cmo --exclude-standard ":(icase)**$SEARCH_QUERY**" 2>/dev/null | LC_ALL=C sed "s/$/:0:0:/g" | colorize_file_output
+    awk '!x[$0]++' <(
+        (git --no-pager grep -I -n -i --untracked "$SEARCH_QUERY" 2>/dev/null | LC_ALL=C sed -E "s/:([0-9]+):/:\1:0:/g" | colorize_output)
+    )
 }
 
 callgrepcommand() {
-	awk '!x[$0]++' <(
-		(find . -type f -ipath "*$SEARCH_QUERY*" | LC_ALL=C sed "s/$/:0:0:/g" | colorize_file_output)
-		(grep -rIHin "$SEARCH_QUERY" . | LC_ALL=C sed -E "s/:([0-9]+):/:\1:0:/g" | colorize_output)
-	)
+    awk '!x[$0]++' <(
+        (find . -type f -ipath "*$SEARCH_QUERY*" 2>/dev/null | LC_ALL=C sed "s/$/:0:0:/g" | colorize_file_output)
+        (grep -rIHin "$SEARCH_QUERY" . 2>/dev/null 2>/dev/null | LC_ALL=C sed -E "s/:([0-9]+):/:\1:0:/g" | colorize_output)
+    )
 }
 
-if (git -C . rev-parse 2>/dev/null) && [ "$2" != 'all' ]; then
-	callgitgrepcommand &
-	trap 'kill -9 $!' SIGPIPE
+if (git -C . rev-parse 2>/dev/null) && [[ "$2" != 'all' ]]; then
+    callgitgrepcommand &
+    trap 'kill -9 $!' SIGPIPE
 else
-	callgrepcommand &
-	trap 'kill -9 $!' SIGPIPE
+    callgrepcommand &
+    trap 'kill -9 $!' SIGPIPE
 fi
